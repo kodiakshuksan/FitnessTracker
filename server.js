c88e4e451e8f3eb9f7d053d05a8759981164ac73
kodiakshuksan <74173891+kodiakshuksan@users.noreply.github.com>
2021-04-25 01:23:56 -0700

    finally seeded the database and started server

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const logger = require("morgan");
 const mongoose = require("mongoose");
-//const db = require("/models");
+//const db = require("../models");
 
 
 
@@ -20,7 +20,8 @@ app.use(express.static("public"));
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", { useNewUrlParser: true });
 
 
-app.use(require("./routes/api.js"));
+//require("./routes/api-routes.js")(app);
+//require("./routes/html-routes.js")(app);
 
 app.listen(PORT, () => {
   console.log(`App running on port ${PORT}!`);
